ec70ec990f26c4833e1abcd27fb17f56be2ffcad
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-11-18 15:27:36 +0100

    changed import

--- src/can/can_handler.py
+++ src/can/can_handler.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from can.i_can_handler import ICanHandler
 from farm_ng.core.event_client import EventClient
 from farm_ng.core.event_service_pb2 import SubscribeRequest
+from farm_ng.core.event_service_pb2 import EventServiceConfig
+from farm_ng.core.event_service_pb2 import EventServiceConfigList
 from farm_ng.core.uri_pb2 import Uri
 from farm_ng.canbus.canbus_pb2 import Twist2d
 from farm_ng.core.events_file_reader import proto_from_json_file
