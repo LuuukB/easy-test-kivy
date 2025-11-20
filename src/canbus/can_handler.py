cc36943793ecc25c1a7658d79042ce559863eb0e
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-11-20 10:41:12 +0100

    both tasks at once

--- src/canbus/can_handler.py
+++ src/canbus/can_handler.py
@@ -66,7 +66,7 @@ class CanHandler(ICanHandler):
                 print("✅ Amiga staat op AUTO READY")
                 rpdo = AmigaRpdo1()
                 rpdo.control_state = AmigaControlState.STATE_AUTO_ACTIVE
-                msg = rpdo.encode()
+                msg = rpdo.to_proto()
                 print("send active")
                 await self.client.request_reply("/can_message", msg, decode=False)
                 print("send message")
