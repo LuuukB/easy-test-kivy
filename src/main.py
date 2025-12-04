22f77b0d99918e9e532acc2e173b795b02405f3a
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-12-04 10:41:36 +0100

    met pijn in mijn hard

--- src/main.py
+++ src/main.py
@@ -71,9 +71,9 @@ class TemplateApp(App):
         # Placeholder task
         #self.async_tasks.append(asyncio.ensure_future(self.template_function()))
         self.async_tasks.append(asyncio.create_task(self.camera_task()))
-        self.async_tasks.append(asyncio.create_task(self.camera_task2()))
-        self.async_tasks.append(asyncio.create_task(self.camera_task3()))
-        self.async_tasks.append(asyncio.create_task(self.camera_task4()))
+        #self.async_tasks.append(asyncio.create_task(self.camera_task2()))
+        #self.async_tasks.append(asyncio.create_task(self.camera_task3()))
+        #self.async_tasks.append(asyncio.create_task(self.camera_task4()))
         #self.async_tasks.append(asyncio.create_task(self.drive_task()))
         #self.async_tasks.append(asyncio.create_task(self.canbus_task()))
         return await asyncio.gather(run_wrapper(), *self.async_tasks)
